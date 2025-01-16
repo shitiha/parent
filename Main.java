47e103d65eda5cc8c501c8efeab508052ede4d58
unknown <n.shitiha@gmail.com>
2025-01-17 00:05:52 +0300

    Добавлен класс Main

--- Main.java
+++ Main.java
@@ -1,27 +1,18 @@
+// Дополнительный метод для демонстрации работы
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        // Создание объекта с константами
-        Time time1 = new Time(1, 30, 45);
-        System.out.println("Время 1: " + time1);
-        System.out.println("Количество секунд: " + time1.calculateSeconds());
+   public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+       System.out.print("Введите номер поезда: ");
+       String trainNumber = scanner.nextLine();
+       System.out.print("Введите направление: ");
+       String direction = scanner.nextLine();
+       System.out.print("Введите время отправления (часы минуты): ");
+       int hours = scanner.nextInt();
+       int minutes = scanner.nextInt();
 
-        // Создание объекта с введенными значениями
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите часы: ");
-        int hours = scanner.nextInt();
-        System.out.print("Введите минуты: ");
-        int minutes = scanner.nextInt();
-        System.out.print("Введите секунды: ");
-        int seconds = scanner.nextInt();
-
-        Time time2 = new Time(hours, minutes, seconds);
-        System.out.println("Время 2: " + time2);
-        System.out.println("Количество секунд: " + time2.calculateSeconds());
-
-        // Увеличение времени на 5 секунд
-        time2.addFiveSeconds();
-        System.out.println("Время 2 после добавления 5 секунд: " + time2);
-    }
+       Name train = new Name(trainNumber, direction, hours, minutes, 0);
+       System.out.println(train.toString());
+   }
 }
